cda5ef41a076ee1a1b0dc58ab84773bce1982353
Tushar Agrawal <72864186+Phantom-TA@users.noreply.github.com>
2025-06-28 14:08:25 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js"
 import cors from "cors"
 const app = express();
-console.log(process.env.CLIENT_URI)
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
